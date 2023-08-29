340c30e88d5043a5cdf98c4b08d1c5375e6f4006
Espenk <espenkleivane@gmail.com>
2023-08-29 22:58:22 +0200

    key

--- zabbix/pythonZabbixApi/run.py
+++ zabbix/pythonZabbixApi/run.py
@@ -2,43 +2,58 @@ import requests
 import json
 import logging
 
-UNAME = None
-PWORD = None
-AUTHTOKEN = None
 
-def read_vault():
-    try:
-        with open("keyvault.json") as fi:
-            data_tmp = json.load(fi)
-            data = data_tmp["keyvault"]
-    except FileNotFoundError as ex:
-        logging.info(ex)
+logging.basicConfig(filename="log.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
-def connect_zabbix(url_zabbix):
-    logging.info("Connection to zabbix, get token")
+class ApiWorker():
+
+    def __init__(self):
+        self.url = None
+        self.user = None
+        self.cred = None
+        self.authtoken = None
+        self.read_vault()
+        self.connect_zabbix()
+
+    def read_vault(self):
+        try:
+            with open("keyvault.json") as fi:
+                data_tmp = json.load(fi)
+                data = data_tmp["keyvault"]
+                rv = data[0]
+                self.url = rv["url"]
+                self.user = rv["user"]
+                self.cred = rv["cred"]
+        except FileNotFoundError as ex:
+            logging.info(ex)
+
+
+    def connect_zabbix(self):
+        logging.info("Connection to zabbix, trying to get token")
+        try:
+            r = requests.post(self.url,
+                          json={
+                              "jsonrpc": "2.0",
+                              "method": "user.login",
+                              "params": {
+                                  "user": self.user,
+                                  "password": self.cred},
+                              "id": 1
+                          })
+
+            # print(json.dumps(r.json(), indent=4, sort_keys=True))
+            self.authtoken = r.json()["result"]
+            logging.info("Authtoken acquired")
+        except Exception as ex:
+            logging.error(ex)
+        return self.authtoken
+
     
-    global UNAME
-    global PWORD
-    global AUTHTOKEN
-    r = requests.post(url_zabbix,
-                  json={
-                      "jsonrpc": "2.0",
-                      "method": "user.login",
-                      "params": {
-                          "user": UNAME,
-                          "password": PWORD},
-                      "id": 1
-                  })
-
-    # print(json.dumps(r.json(), indent=4, sort_keys=True))
-    AUTHTOKEN = r.json()["result"]
-    logging.info(AUTHTOKEN)
-    return AUTHTOKEN
-
-def main():
-    read_vault()
-
 
 if __name__ == "__main__":
-    main()
-   
+    worker = ApiWorker()
+    print(worker.cred)
