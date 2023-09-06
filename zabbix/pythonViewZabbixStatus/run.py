e99d8f5d572854cd269464432175daae3f5939e3
Espenk <espenkleivane@gmail.com>
2023-09-06 13:10:49 +0200

    zab

---
+++ zabbix/pythonViewZabbixStatus/run.py
@@ -0,0 +1,8 @@
+# https://www.zabbix.com/documentation/current/en/manual/api/reference/problem/get
+
+# "jsonrpc": "2.0",
+#    "method": "problem.get",
+#    "params": {
+
+# problem.get - by default return "unresolved problems" this means "open problems"
+
