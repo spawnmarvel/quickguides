4d727f8356dbaa763835f330e086869ef5d8914f
Espenk <espenkleivane@gmail.com>
2024-03-20 22:24:42 +0100

    mqtt

--- amqp_mqtt_python/run_pub_sensor1.py
+++ amqp_mqtt_python/run_pub_sensor1.py
@@ -39,7 +39,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(MQTTTOPIC)
+    # client.subscribe(MQTTTOPIC)
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
