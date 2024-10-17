bba75eee73ce9443cd9b28947184533577522321
Espenk <espenkleivane@gmail.com>
2024-10-17 11:37:54 +0200

    sql

--- sqlplus/efficent_aspen_queries.sql
+++ sqlplus/efficent_aspen_queries.sql
@@ -1,6 +1,6 @@
-Each database transaction, whether read or write, locks the entire Aspen InfoPlus.21 database for the duration of the transaction.  This means that while a query accesses the database, other queries (and indeed all other Aspen InfoPlus.21 applications) have to wait.
+-- Each database transaction, whether read or write, locks the entire Aspen InfoPlus.21 database for the duration of the transaction.  This means that while a query accesses the database, other queries (and indeed all other Aspen InfoPlus.21 applications) have to wait.
 
-Consider the following query:
+-- Consider the following query:
 
 write atcai.name||' '||
 
@@ -12,11 +12,11 @@ write atcai.name||' '||
 
  
 
-Since each direct field reference (i.e. atcai.name, atcai.ip_description, atcai.ip_input_value, and atcai.ip_eng_units) is a database read, therefore the query has four separate database accesses.
+-- Since each direct field reference (i.e. atcai.name, atcai.ip_description, atcai.ip_input_value, and atcai.ip_eng_units) is a database read, therefore the query has four separate database accesses.
 
  
 
-Compare that query with
+-- Compare that query with
 
  
 
@@ -28,15 +28,15 @@ select name, ip_description, ip_input_value, ip_eng_units
 
  
 
-The second query replaces four direct field references with one select statement that fetches the fields name, ip_description, ip_input_value, and ip_eng_units from atcai.  The select statement is able to read all four fields with one database transaction.
+-- The second query replaces four direct field references with one select statement that fetches the fields name, ip_description, ip_input_value, and ip_eng_units from atcai.  The select statement is able to read all four fields with one database transaction.
 
  
 
-The same principle is true with indirect field references.
+-- The same principle is true with indirect field references.
 
  
 
-The query
+-- The query
 
  
 
@@ -56,7 +56,7 @@ write recname->name||' '||
 
  
 
-has four database transactions while the query
+-- has four database transactions while the query
 
  
 
@@ -72,19 +72,19 @@ select name, ip_description, ip_input_value, ip_eng_units
 
  
 
-has only one database read.
+-- has only one database read.
 
  
 
-·         Reduce the use of Write Statements
+-- ·         Reduce the use of Write Statements
 
-A simple way to reduce the number of database transactions and the number of statements processed is to eliminate write statements.  Many programmers leave diagnostic write statements in queries forgetting to remove the writes when they are no longer needed.
+-- A simple way to reduce the number of database transactions and the number of statements processed is to eliminate write statements.  Many programmers leave diagnostic write statements in queries forgetting to remove the writes when they are no longer needed.
 
  
 
-·         Disable logging statements
+-- ·         Disable logging statements
 
-Related to reducing the number of write statements is disabling logging statements but using the command
+-- Related to reducing the number of write statements is disabling logging statements but using the command
 
  
 
@@ -92,17 +92,17 @@ set log_rows = 0;
 
  
 
-at the beginning of the query.
+-- at the beginning of the query.
 
  
 
-·         Avoid like comparisons when accessing a specific record
+-- ·         Avoid like comparisons when accessing a specific record
 
-When accessing specific records, use = instead of like in where clauses.
+-- When accessing specific records, use = instead of like in where clauses.
 
  
 
-For example, the query
+-- For example, the query
 
  
 
@@ -112,11 +112,11 @@ where name like 'ATCAI';
 
  
 
-causes the query to look at each record defined by IP_AnalogDef to determine if its name equals 'ATCAI'.
+-- causes the query to look at each record defined by IP_AnalogDef to determine if its name equals 'ATCAI'.
 
  
 
-whereas the query
+-- whereas the query
 
  
 
@@ -124,15 +124,15 @@ select name, ip_input_time, ip_input_value from ATCAI;
 
  
 
-causes the query to call an Aspen InfoPlus.21 API routine to translate 'ATCAI' into a record ID followed by another API routine to directly read the appropriate fields using the record ID.  In this case, the query accesses only one record instead of looping through all tags defined by IP _AnalogDef looking for names that match the string specified in the like statement.
+-- causes the query to call an Aspen InfoPlus.21 API routine to translate 'ATCAI' into a record ID followed by another API routine to directly read the appropriate fields using the record ID.  In this case, the query accesses only one record instead of looping through all tags defined by IP _AnalogDef looking for names that match the string specified in the like statement.
 
-·         Use Temporary Tables
+-- ·         Use Temporary Tables
 
-Use temporary tables in queries to minimize repeated file or Aspen InfoPlus.21 database accesses.  The IQ task executing a query stores temporary tables in memory. So access to information in a temporary table is faster than querying from the database, or reading from a file.
+-- Use temporary tables in queries to minimize repeated file or Aspen InfoPlus.21 database accesses.  The IQ task executing a query stores temporary tables in memory. So access to information in a temporary table is faster than querying from the database, or reading from a file.
 
-For queries driven by data in text files, read all of the lines (parsing out the individual values) into temporary tables and then process the data from the temporary table instead of reading values one at a time from one line at a time.
+-- For queries driven by data in text files, read all of the lines (parsing out the individual values) into temporary tables and then process the data from the temporary table instead of reading values one at a time from one line at a time.
 
-For example, consider replacing
+-- For example, consider replacing
 
 iFC = (Select Count(*) From '&nFile');
 
@@ -154,13 +154,13 @@ FOR I = 2 To iFC DO
 
        FROM '&nFile' Where linenum = i);
 
-  < ... Processing ... >
+--  < ... Processing ... >
 
 END
 
  
 
-with
+-- with
 
  
 
@@ -190,21 +190,22 @@ Insert Into module.tcrf
 
 For (Select * From module.tcrf) Do
 
-  < ... Processing ... >
+  -- < ... Processing ... >
 
 End
 
  
 
-When a query selects the same information from Aspen InfoPlus.21 multiple times (this happens frequently, for example, when using sub-queries to filter information for a main query), select data from Aspen InfoPlus.21 into a temporary table and then do subsequent selects from the temporary table.
+-- When a query selects the same information from Aspen InfoPlus.21 multiple times (this happens frequently, for example, when using sub-queries to filter information for a main query), select data from Aspen InfoPlus.21 into a temporary table and then do subsequent selects from the temporary table.
 
-For example, consider replacing
+
+--- For example, consider replacing
 
 FOR SELECT COUNT(name) AS CNT FROM IP_ANALOGDEF WHERE NAME = TKNAME DO
 
        IF CNT = 1 THEN
 
-         < ... Processing ... >
+--         < ... Processing ... >
 
        END
 
@@ -212,7 +213,7 @@ END
 
  
 
-with
+-- with
 
 Declare Local Temporary Table module.TankNames (TankName Char(24));
 
@@ -224,7 +225,7 @@ Insert Into module.TankNames
 
  
 
-  < ... Processing ... >
+ -- < ... Processing ... >
 
  
 
@@ -236,21 +237,21 @@ If Exists (Select TankName
 
 Then
 
-  < ... Processing ... >
+ -- < ... Processing ... >
 
 End
 
  
 
-·         Combine Select Statements with For Loops to Initialize Local Variables with Fields from Records
+-- ·         Combine Select Statements with For Loops to Initialize Local Variables with Fields from Records
 
  
 
-Combining select statements with for loops to initialize local variables with fields from records can significantly speed a query.
+-- Combining select statements with for loops to initialize local variables with fields from records can significantly speed a query.
 
  
 
-For example, the following query takes four database accesses to initialize the local variables HiHi, Hi, Lo, and LoLo since each direct field reference is a database transaction:
+-- For example, the following query takes four database accesses to initialize the local variables HiHi, Hi, Lo, and LoLo since each direct field reference is a database transaction:
 
  
 
@@ -268,7 +269,7 @@ LoLo = ATCAI.IP_Low_Low_Limit;
 
  
 
-By combining a select statement with a for loop, you can initialize the same four variables with only one read to the database as the following example shows:
+--- By combining a select statement with a for loop, you can initialize the same four variables with only one read to the database as the following example shows:
 
  
 
