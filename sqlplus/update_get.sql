5ac8d5c686dd174e46a0587f69f1d520f2de4a6c
Espenk <espenkleivane@gmail.com>
2025-01-28 12:49:11 +0100

    sql

--- sqlplus/update_get.sql
+++ sqlplus/update_get.sql
@@ -6,7 +6,7 @@ SUBSTRING(2 OF LINE BETWEEN ';') AS IP21TAG
 from 'c:\temp\tags\new_opc.txt') do
 
 	for (select name as nn, io_tagname as ionn, occnum as onum, "IO_VALUE_RECORD&&FLD" as tag, io_data_status as st
-		from iolongtaggetdef where trim(replace('IP_INPUT_VALUE' with '' in "IO_VALUE_RECORD&&FLD" )) like IP21TAG ) do
+		from iolongtaggetdef where trim(replace('IP_INPUT_VALUE' with '' in "IO_VALUE_RECORD&&FLD" )) = IP21TAG ) do
 
 		--from iolongtaggetdef where trim(replace('IP_INPUT_VALUE' with '' in "IO_VALUE_RECORD&&FLD" )) like IP21TAG and name = 'get recname') do
 	
