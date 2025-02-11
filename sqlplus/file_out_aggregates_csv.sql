2d27f57ce660c67b79eee4cf9798c795765de53c
Espenk <espenkleivane@gmail.com>
2025-02-11 09:48:49 +0100

    restart

--- sqlplus/file_out_aggregates_csv.sql
+++
@@ -1,9 +0,0 @@
-SET OUTPUT 'c:\temp\files\extract-tagname.csv'; -- remove -- if you want to write to a file
-
-for(select name n, ts t, avg av from aggregates where name = 'tagname'
-	and ts between '30-JAN-18 00:00' AND '20-NOV-24 00:00'
-      and period = 24:00)do
-
-	write n||';'||t||';'||av;
-
-end
--- sqlplus/file_out_append.sql
+++
@@ -1,8 +0,0 @@
-SET OUTPUT 'c:\temp\fil.txt';--skriver over
-SET APPEND 'c:\temp\fil.txt'; legger til
-Select name, ip_input_time, ip_input_value from ip_danalogdef where name ="TEST-TAG";
-
-
-local logfile char(300);
-logfile = 'C:\temp\DOF\RealTime_'|| TRIM(REPLACE(':' WITH '_' IN GETDBTIME)) || '.log';
-SET OUTPUT logfile;
