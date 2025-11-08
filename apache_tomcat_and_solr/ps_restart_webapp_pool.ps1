264a1772238c116eacb2c1b0bc2e2b48fe49a3c9
Espenk <espenkleivane@gmail.com>
2025-11-08 09:43:12 +0100

    swap

--- apache_tomcat_and_solr/ps_restart_webapp_pool.ps1
+++ apache_tomcat_and_solr/ps_restart_webapp_pool.ps1
@@ -2,3 +2,5 @@ Import-Module WebAdministration
 Get-IISAppPool -Name "SolrAppPool"
 Restart-WebAppPool -Name "SolrAppPool"
 
+# Swap space goes down after 1 min to normal 15gb
+# Run saturday, 64 bit, not 32 bit ps1
